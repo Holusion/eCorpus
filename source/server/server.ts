Holusion/eCorpus

commit 312c955008bf84f6162853e7ac894bf11afe906c

better help for failed login. Prevent WWW-Authenticate popup to show on login fail

source/ui/state/auth.ts
@@ -20,6 +20,7 @@ export function setSession(s ?:UserSession){
 export async function doLogin(username :string, password :string) :Promise<void>{
   await fetch("/api/v1/login", {
     method: "POST",
+    credentials:'omit', //Prevent the browser from showing an authentification popup
     headers:{
       "Content-Type":"application/json",
       "Accept": "application/json",

source/server/routes/api/v1/login.ts
@@ -1,7 +1,7 @@
 import { createHmac } from "crypto";
 import { Request, RequestHandler, Response } from "express";
 import User, { SafeUser } from "../../../auth/User.js";
-import { BadRequestError, ForbiddenError, HTTPError } from "../../../utils/errors.js";
+import { BadRequestError, ForbiddenError, NotFoundError, UnauthorizedError } from "../../../utils/errors.js";
 import { AppLocals, getHost, getUser, getUserManager } from "../../../utils/locals.js";
 import sendmail from "../../../utils/mails/send.js";
 /**
@@ -17,7 +17,14 @@ export const postLogin :RequestHandler = (req, res, next)=>{
     let safeUser = User.safe(user);
     Object.assign(req.session as any, safeUser);
     res.status(200).send({...safeUser, code: 200, message: "OK"});
-  }, next);
+
+  }, (e)=>{
+    if(e instanceof NotFoundError){
+      next(new UnauthorizedError(`username ${username} not found`));
+    }else{
+      next(e);
+    }
+  });
 };
 
 export async function getLogin(req :Request, res:Response){

source/ui/composants/UserLogin.ts
@@ -21,6 +21,9 @@ import Notification from "./Notification";
 
     @property({type:Boolean})
     active = false;
+
+    @property({attribute:false})
+    error :string = "";
     
     constructor()
     {
@@ -36,9 +39,11 @@ import Notification from "./Notification";
       .then(()=>{
         console.log("User logged-in succesfully");
         this.dispatchEvent(new CustomEvent("close"));
+        this.error = "";
       },(e)=>{
         console.log("Login failed :", e);
-        Notification.show(`Failed to login : ${e}`, "warning")
+        Notification.show(`Failed to login : ${e}`, "warning", 4000);
+        this.error = e.message.replace(/Error: \[\d+\]\s?/, "");
       }).finally(()=> this.active = false)
     }
 
@@ -75,6 +80,9 @@ import Notification from "./Notification";
             <input type="password" autocomplete="current-password" name="password" id="password" placeholder="${this.t("ui.password")}" required>
             <label for="password">${this.t("ui.password")}</label>
           </div>
+        </div>
+        <div>
+          <span class="text-error">${this.error}</span>
         </div>
         <div class="form-group">
           <div class="form-item">

source/ui/styles/common.scss
@@ -15,3 +15,16 @@
     }
   }
 }
+
+.text-info {
+  color: $color-info;
+}
+.text-success {
+  color: $color-success; 
+}
+.text-warning{
+  color: $color-warning; 
+}
+.text-error {
+  color: $color-error; 
+}

commit b07591f434f987d130da411f480f69d7b0fb03fa

fix login bug introduced in 312c955008bf84f6162853e7ac894bf11afe906c

source/server/server.ts
@@ -209,10 +209,14 @@ export default async function createServer(config = defaultConfig) :Promise<expr
     }
     let code = (error instanceof HTTPError )? error.code : 500;
 
-    if(code === 401 && !(req.get("Accept")?.startsWith("text/html") && req.get("User-Agent")?.startsWith("Mozilla"))){
-      //We try to NOT send the header for browser requests because we prefer the HTML login to the browser's popup
+    if(code === 401 
+    //We try to NOT send the header for browser requests because we prefer the HTML login to the browser's popup
       //Browser tends to prefer text/html and always send Mozilla/5.0 at the beginning of their user-agent
       //If someone has customized their headers, they'll get the ugly popup and live with it.
+      && !(req.get("Accept")?.startsWith("text/html") && req.get("User-Agent")?.startsWith("Mozilla"))
+      //Also don't apply it for login route because it doesn't make any sense.
+      && req.path !== "/api/v1/login"
+    ){
       res.set("WWW-Authenticate", "Basic realm=\"authenticated access\"");
     }
 

source/ui/state/auth.ts
@@ -20,7 +20,6 @@ export function setSession(s ?:UserSession){
 export async function doLogin(username :string, password :string) :Promise<void>{
   await fetch("/api/v1/login", {
     method: "POST",
-    credentials:'omit', //Prevent the browser from showing an authentification popup
     headers:{
       "Content-Type":"application/json",
       "Accept": "application/json",