19cae05ab56a3d99b91dca89e1522f7c00519368
Sebastien DUMETZ <s.dumetz@holusion.com>
2024-01-30 16:41:34 +0100

    allow larger JSON scene files upload

--- source/server/routes/scenes/index.ts
+++ source/server/routes/scenes/index.ts
@@ -34,7 +34,7 @@ router.delete("/:scene", isAdministrator, wrap(handleDeleteScene));
 router.get("/:scene/:file(*.svx.json)", wrap(handleGetDocument));
 router.put("/:scene/:file(*.svx.json)", 
   canWrite,
-  bodyParser.json({type:["application/si-dpo-3d.document+json", "application/json"]}),
+  bodyParser.json({type:["application/si-dpo-3d.document+json", "application/json"], limit: 204800}),
   wrap(handlePutDocument)
 );
 router.copy("/:scene/:file(*.svx.json)", canWrite, wrap(handleCopyDocument));
