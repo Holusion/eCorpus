9e06955a96a668233b052cdd0fdbe330f26a0e65
Sebastien DUMETZ <s.dumetz@holusion.com>
2024-02-22 11:39:36 +0100

    fix mobile layout where possible. Use grid for the main page layout

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,9 +2,8 @@
 
 
 function register_dropdowns(){
-  const dropdowns = document.querySelectorAll(".dropdown-toggle[data-target]");
+  const dropdowns = document.querySelectorAll(".dropdown-toggle[data-target], .navbar-toggle[data-target]");
   function onDropdownToggle(){
-    console.log("Toggle dropdown", this)
     const target = document.querySelector(this.dataset.target);
     if(!target) return;
     const expand = this["ariaExpanded"] !== "true";
