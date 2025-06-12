fc2fb4736515bb0b9fc312ef1483a8d036628a23
Inao0 <37711386+Inao0@users.noreply.github.com>
2025-06-12 16:49:16 +0200

    WIP : deadlock on multiple glb (zip?)  file upload

--- source/ui/composants/UploadForm.ts
+++ source/ui/composants/UploadForm.ts
@@ -4,12 +4,14 @@ import { customElement, state } from "lit/decorators.js";
 import Notification from "./Notification";
 
 import styles from '!lit-css-loader?{"specifier":"lit"}!sass-loader!../styles/common.scss';
+import i18n from "../state/translate";
 
 interface UploadOperation{
   //Unique ID of the upload. Might be different from "name" when we upload a scene zip
   id: string;
+  filename: string;
   //An array to be able to show a list of imported scenes in case of zip uploads
-  names: string[];
+  scenes: {name: string, status?: "ok"|"fail"}[];
   error ?:{code?:number, message:string};
   done :boolean;
   total ?:number;
@@ -18,7 +20,7 @@ interface UploadOperation{
 }
 
 @customElement("upload-form")
-export default class UploadForm extends LitElement{
+export default class UploadForm extends i18n(LitElement){
   static shadowRootOptions = {...LitElement.shadowRootOptions, delegatesFocus: true};
 
   /**
@@ -51,10 +53,7 @@ export default class UploadForm extends LitElement{
   /**
    * @returns True if the file upload started. False otherwise
    */
-  upload(data :FormData) :boolean{
-    const file = data.get("files") as File;
-    let name = data.get("name") as string;
-    let language = data.get("language");
+  upload(file: File, language, name: string) :boolean{
 
     const as_scenes = file.name.endsWith(".zip");
 
@@ -88,7 +87,8 @@ export default class UploadForm extends LitElement{
     //Create the download operation
     this.uploads = [ ...this.uploads, {
       id,
-      names:[name],
+      filename: file.name,
+      scenes:[{name}],
       progress:0,
       done: false,
       abort: ()=>{
@@ -103,10 +103,13 @@ export default class UploadForm extends LitElement{
       console.log("DONE");
       if(as_scenes){
         try{
-          let response = JSON.parse(xhr.responseText) as {ok:string[], fail:[]};
-          let scenes = Array.from(new Set(response.ok.map(name=>name.split("/")[0])));
-          this.splice(id, {done: true, names: scenes});
-        }catch(e){
+          console.log (JSON.parse(xhr.responseText) as {ok:string[], fail: string[]});
+          let response = JSON.parse(xhr.responseText) as {ok:string[], fail: string[]};
+          let scenes: {name: string; status?: "ok" | "fail";}[]= Array.from(new Set(response.ok.map(name=> name.split("/")[0]))).map((name)=> {return {name: name, status:"ok"}});
+          let failedScenes: {name: string; status?: "ok" | "fail";}[] = Array.from(new Set(response.fail.map(name=> name.split("/")[0]))).map((name) => {return {name: name, status: "fail"}});
+          this.splice(id, {done: true, scenes: [...scenes, ...failedScenes]});
+        } catch(e){
+          console.log("error", e);
           setError({ message: e.message})
         }
       }else{
@@ -136,60 +139,117 @@ export default class UploadForm extends LitElement{
     ev.stopPropagation();
     const data = new FormData(ev.target as HTMLFormElement);
 
-    this.upload(data);
+    const files =  data.getAll("files").map((fileData) => {return fileData as File});
+
+    files.forEach((file) => this.upload(file, data.get("language"), files.length==1? data.get("name") as string: ""));
     (ev.target as HTMLFormElement).reset();
     return false;
   }
 
-  protected render(): unknown {
-    return html`${this.uploads.length? html`
-      <section class="container upload-status-container" aria-label="uploads">
-        ${this.uploads.map( ({id, names, progress, total, done, abort, error}) => {
-          let name = names.join(", ");
-          // console.log("Render upload : ", id, names, error, done);
-          let content = html`<progress id="progress-${name}" max="100" value=${progress}></progress>`;
-          if(error){
-            content = html`<span id="progress-${name}" class="text-error progress">${error.message}</span>`;
-          }else if(done){
-            content = html`<span id="progress-${name}" class="progress">
-              ${names.map(name=>html`<a href="/ui/scenes/${encodeURIComponent(name)}">${name}</a>`)}
-            </span>`;
-          }
+    protected renderSceneBox(id, name, status) {
+      console.log (id, name, status);
+      return status=="ok" ? html`
+        <a href="/ui/scenes/${encodeURI(name)}" class="card-header">
+         <ui-icon name="list"></ui-icon><span> ${name}</span>
+        </a>
+        <a class="tool-link" href="/ui/scenes/${name}/view" >
+        <ui-icon name="eye"></ui-icon>
+        <span class="tool-text">${this.t("ui.view",{capitalize: "string"})}</span>
+                <a class="tool-link" href="/ui/scenes/${name}/edit">
+        <ui-icon name="edit"></ui-icon>
+          <span class="tool-text">${this.t("ui.edit",{capitalize: "string"})}</span>
+        </a>
+      </a>` :
+      html`<span class="text-error error-message progress">
+      ${name} failed. Probably due to a scene already existing with the same name as the glb uploaded
+      </span>`;
+    }
 
+
+  protected render(): unknown {
+    return html`      <slot @submit=${this.handleSubmit}></slot>
+    
+    ${this.uploads.length? html`
+      <section class="container upload-status-container" aria-label="uploads">
+        ${this.uploads.map( ({id, filename, scenes, progress, total, done, abort, error}) => {
           const handleAbort = ()=>{
             if(!done && !error) abort();
             else this.splice(id);
           }
-
-          return html`<div role="status" id="upload-${id}" class="upload-status">
-            <label for="progress-${name}">${name}</label>
-            <span> ${total?html`(<b-size b=${total}></b-size>)`:null}</span>
-            ${content}
-            <button class="btn btn-transparent btn-inline btn-small text-error" @click=${handleAbort}>🗙</button>
+          let content = html`
+          <div role="status" id="upload-${id}" class="upload-status">
+          <progress id="progress-${filename}" max="100" value=${progress}></progress>
           </div>`
+          if(error){
+            content = html`<span id="progress-${filename}" class="text-error progress">${scenes.map((scene)=> scene.name).join(',')}: ${error.message}</span>`;
+          }else if(done){
+            content = 
+            html`<div class="scenes-container">${scenes.map(scene => this.renderSceneBox(id, scene.name, scene.status || (error? "fail":"ok")))}
+            </div>`
+          }
+
+          return html`<div class="filename"><label for="progress-${filename}">${filename}</label></div>
+            <div><span> ${total?html`(<b-size b=${total}></b-size>)`:null}</span></div>
+            ${content}
+            <button class="btn btn-transparent btn-inline btn-small text-error" @click=${handleAbort}>🗙</button>`; 
+
         })}
       </section>`: null}
-      <slot @submit=${this.handleSubmit}></slot>
     `;
   }
 
   static styles = [styles, css`
     .upload-status-container{
-      display: flex;
-      flex-direction: column;
-      gap: .5rem;
+      display: grid;
+      grid-template-columns: 1fr max-content 1fr max-content;
+      gap: 1em;
+      align-items: center;
     }
-      
-    .upload-status{
-      display:flex;
-      gap: 1rem;
-      background: var(--color-element);
-      padding: .2rem;
+    .container .filename {
+      word-breaK:break-all;
+    }
 
+    .scenes-container{
+      display: grid;
+      grid-template-columns: 1fr max-content max-content;
+      gap: 0.5em;
+    }
+
+    .upload-status{
+      padding: .2rem;
+    }
+
+    .error-message{
+      grid-column: 1 / 4;
     }
     
     progress, .progress{
       flex: 1 1 auto;
+      width: 100%;
+    }
+
+    progress, 
+    
+    .scenes-container a {
+      /*padding: 0.75em;*/
+    	align-content: flex-start;
+	    display: flex;
+      font-weight: bolder;
+    }
+
+    .scenes-container a:hover{
+      color: var(--color-secondary-light);
+    }
+
+    .ui-icon {
+      fill: currentColor;
+    	flex: 0 0 auto;
+    	display: inline-block;
+    	height: 1rem;
+    	width: 1rem;
+    	height: 1lh;
+	    width: 1lh;
+      margin-right: 0.3em;
     }
   `];
 }
