05205176e961110ae4af19c26f639b36975a21e6
Sebastien DUMETZ <s.dumetz@holusion.com>
2025-07-31 12:40:46 +0200

    better-tested migrations logic with additional methods to ease decomposition

--- source/server/vfs/helpers/migrations.ts
+++ source/server/vfs/helpers/migrations.ts
@@ -10,41 +10,80 @@ import { DatabaseHandle } from './db.js';
 const debug = debuglog("pg:migration");
 
 export interface MigrationParams{
-  db: DatabaseHandle,
+  db: ClientBase,
   migrations: string,
   force: boolean,
 }
 
+export interface MigrationFile{
+  id: number;
+  name: string;
+  filepath: string;
+}
 
-async function listMigrations(dir:string):Promise<{id:number, name:string, filepath:string}[]>{
-  try{
-    return (await readdir(dir))
-    .map(x => x.match(/^(\d+).(.*?)\.sql$/))
-    .filter(x => x !== null)
-    .map((x:any) => ({ id: Number(x[1]), name: x[2], filepath: path.join(dir, x[0]) }))
-    .sort((a, b) => Math.sign(a.id - b.id));
-  }catch(e: any){
-    if(e.code == "ENOTDIR"){
-      throw new Error(`Could not find migrations directory at ${dir}`)
-    }
-    throw e;
+interface MigrationRecord{
+  id:number;
+  name:string;
+  down:string;
+}
+
+
+function *_streamMapMigrations(files :string[]){
+  for(let filepath of files){
+    const basename = path.basename(filepath);
+    const m = basename.match(/^(\d+).(.*?)\.sql$/);
+    if(!m) continue;
+    yield { id: Number(m[1]), name: m[2], filepath}
   }
 }
 
-async function parseMigration(file:string){
-  const content = await readFile(file, {encoding: "utf-8"});
-  const [up, down] = content.split(/^--\s+?down\b/im)
+export function mapMigrations(files :string[]){
+  return Array.from(_streamMapMigrations(files)).sort((a, b) => Math.sign(a.id - b.id))
+}
 
+/**
+ * Makes a sorted list of migrations found in a directory
+ * @param dir 
+ * @returns 
+ */
+export async function listMigrations(dir:string):Promise<{id:number, name:string, filepath:string}[]>{
+  let files = await readdir(dir);
+  let migrations = mapMigrations( files.map(f=>path.join(dir,f)) );
+  if(migrations.length == 0) throw new Error(`No migration files found in ${dir}`);
+  return migrations;
+}
+
+export function parseMigrationString(content :string){
+  const [up, down, ...rest] = content.split(/^--+\s*?down\b/im)
+  if(rest.length) throw new Error(`"down" migration delimiter matched multiple times`);
   return {
-    up: up.replace(/^-- .*?$/gm, '').trim(),
+    up: up.trim(),
     down: down ? down.trim() : '',
+  }
+}
+
+export async function parseMigrationFile(file:string){
+  const content = await readFile(file, {encoding: "utf-8"});
+  return parseMigrationString(content);
+}
+
+async function undoMigration(db:ClientBase, record :MigrationRecord){
+  debug(`Undo migration ${record.id.toString(10).padStart(3, "0")}-${record.name} (file not found)`);
+  await db.query("BEGIN TRANSACTION");
+  try{
+    await db.query("SET CONSTRAINTS ALL DEFERRED");
+    await db.query(record.down);
+    await db.query(`DELETE FROM migrations WHERE id = $1`, [record.id]);
+  }catch(e){
+    throw expandSQLError(e, record.down, `migration record ${record.id.toString().padStart(3, "0")}-${record.name}`);
+  }finally{
+    await db.query("END TRANSACTION");
   }
 }
 
 export async function migrate({db, migrations: dir, force}: MigrationParams){
   const files = await listMigrations(dir);
-  if(files.length == 0) throw new Error(`No migration files found in ${dir}`);
-  await db.run(`
+  await db.query(`
     CREATE TABLE IF NOT EXISTS "migrations" (
     id   INTEGER PRIMARY KEY,
     name TEXT    NOT NULL,
@@ -52,38 +91,20 @@ export async function migrate({db, migrations: dir, force}: MigrationParams){
     down TEXT    NOT NULL
   );`);
 
-  let records:{id:number, name:string, down:string}[] = (await db.all(`SELECT id, name, down FROM "migrations" ORDER BY id ASC`));
-  
+  let records:{id:number, name:string, down:string}[] = (await db.query(`SELECT id, name, down FROM "migrations" ORDER BY id ASC`)).rows;
+  await db.query("BEGIN TRANSACTION");
   try{
     //Undo any migration that has no file
     for(const record of records.slice().reverse()){
       if(files.find(m=>m.id === record.id)) continue;
-      debug(`Undo migration ${record.id.toString(10).padStart(3, "0")}-${record.name} (file not found)`);
-      try{
-        await db.beginTransaction(async (tr)=>{
-          await tr.run("SET CONSTRAINTS ALL DEFERRED");
-          await tr.run(record.down);
-        });
-      }catch(e){
-        throw expandSQLError(e, record.down, `migration record ${record.id.toString().padStart(3, "0")}-${record.name}`);
-      }
+      await undoMigration(db, record);
       records = records.filter(r=>r.id!== record.id);
     }
 
     if(force && records.length){
       const lastMigration = records[records.length - 1];
       if(lastMigration){
-        debug(`Undo migration ${lastMigration.id.toString(10).padStart(3, "0")}-${lastMigration.name} (forced)`);
-        try{
-          await db.beginTransaction(async (tr)=>{
-            await tr.run("SET CONSTRAINTS ALL DEFERRED");
-            await tr.run(lastMigration.down);
-            await tr.run(`DELETE FROM migrations WHERE id = $1`, [lastMigration.id]);
-          });
-
-        }catch(e){
-          throw expandSQLError(e, lastMigration.down, `migration record ${lastMigration.id.toString().padStart(3, "0")}-${lastMigration.name}`);
-        }
+        await undoMigration(db, lastMigration);
         records = records.slice(0, -1);
       }
     }
@@ -91,27 +112,36 @@ export async function migrate({db, migrations: dir, force}: MigrationParams){
     const lastMigrationId = records.length? records[records.length -1].id :0;
     for(let file of files){
       if(file.id <= lastMigrationId) continue;
-      const m = await parseMigration(file.filepath);
+      const m = await parseMigrationFile(file.filepath);
       debug(`Apply migration ${path.basename(file.filepath)}`);
+      await db.query("BEGIN TRANSACTION");
       try{
-        await db.beginTransaction(async (tr)=>{
-          await tr.run("SET CONSTRAINTS ALL DEFERRED");
-          await tr.run(m.up);
-        });
-        await db.run(`
-          INSERT 
-          INTO migrations 
-            (id, name, up, down)
-          VALUES
-            ($1, $2, $3, $4)
-          `, [file.id, file.name, m.up, m.down]);
+          await db.query("SET CONSTRAINTS ALL DEFERRED");
+          await db.query(m.up);
+          await db.query(`
+            INSERT 
+            INTO migrations 
+              (id, name, up, down)
+            VALUES
+              ($1, $2, $3, $4)
+            `, [file.id, file.name, m.up, m.down]);
       }catch(e:any){
         throw expandSQLError(e, m.up, `file ${path.basename(file.filepath)}`);
+      }finally{
+          await db.query("END TRANSACTION");
       }
       
     }
+    await db.query("COMMIT TRANSACTION");
   }catch(e:any){
-    if(e.detail && debug.enabled) debug(`Erreur de migration : ${e.message}\n\t${e.detail}${e.hint?"\n\t"+e.hint:""}`);
+    try{
+      await db.query("ROLLBACK TRANSACTION");
+      /* c8 ignore start */
+    }catch(e){ 
+      console.warn("Failed to rollback migration transaction :", e);
+    }
+    if(e.detail && debug.enabled) debug(`Databse migration error: ${e.message}\n\t${e.detail}${e.hint?"\n\t"+e.hint:""}`);
+    /* c8 ignore stop */
     throw e;
   }
 }
