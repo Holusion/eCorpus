0f30565c943bdc7ee453cb0174771e1384e22756
Sebastien DUMETZ <s.dumetz@holusion.com>
2025-09-01 10:58:33 +0200

    partial rewrite of DB cleanup methods for more clarity

--- source/server/vfs/Clean.ts
+++ source/server/vfs/Clean.ts
@@ -2,34 +2,48 @@ import fs from "fs/promises";
 import timers from "timers/promises";
 import {constants} from "fs";
 import BaseVfs from "./Base.js";
-import FilesVfs from "./Files.js";
+import { randomInt } from "crypto";
+import Vfs from "./index.js";
 
 
-
+/**
+ * Cleaning routines. Main entrypoint should be the `clean` method
+ * Each routine should have internal delays that prevents it from causing resource contention on the server
+ */
 export default abstract class CleanVfs extends BaseVfs{
   /**
    * performs routine cleanup tasks
    * A reasonable delay is added where necessary to ensure the server can continue running properly
+   * Delays are randomized to prevent too much contention when starting multiple instances on the same server
    */
   public async clean(){
     let cleanups = [
       this.cleanLooseObjects,
       this.checkForMissingObjects,
     ];
+      await timers.setTimeout(randomInt(100));
     for (let fn of cleanups){
-      await fn.call(this);
-      await timers.setTimeout(600 + Math.random()*1000);
+      await fn.call(this as any as Vfs);
+      await timers.setTimeout(randomInt(100, 500));
     }
   }
 
-  protected async cleanLooseObjects(){
+  /**
+   * @fixme this method is known to be slightly unsafe because it _could_ clean an object that is unreferenced when scanning but become referenced before it is removed.
+   */
+  protected async cleanLooseObjects(this:Vfs){
     let it = await fs.opendir(this.objectsDir);
     let loose = [];
     for await (let object of it){
-        let row = await this.db.get(`SELECT COUNT(file_id) AS count FROM files WHERE hash = $1`, [object.name]);
-        if(!row || row.count == 0 ){
-            loose.push(object.name);
+      await timers.setTimeout(randomInt(1));
+      let row = await this.db.get(`SELECT COUNT(file_id) AS count FROM files WHERE hash = $1`, [object.name]);
+      if(!row || row.count == 0 ){
+        //try to prevent race conditions by ensuring file is old enough
+        const stat = await fs.stat(this.getPath({hash: row.hash}));
+        if(stat.mtime.valueOf() < Date.now() - 3600){
+          loose.push(object.name);
         }
+      }
     }
     //Loose objects are OK : wan may delete scenes which will remove all history
     if(loose.length)console.log("Cleaning %d loose objects", loose.length);
@@ -39,24 +53,25 @@ export default abstract class CleanVfs extends BaseVfs{
   }
 
   public async optimize(){
-    /** @fixme unused */ 
+    // autovacuum is enabled at the cluster level, which prevents scheduling conflicts with multiple DBs
+    // the autovacuum daemon will also issue an ANALYZE query when table data has changed significantly.
   }
 
   /**
    * Utility function used mainly for debugging to check if some objects are referenced but not present on disk
    */
   protected async checkForMissingObjects(){
-    let objects = await this.db.all(`SELECT DISTINCT hash AS hash FROM files WHERE data IS NULL`);
+
     let missing = 0;
-    for(let object of objects){
+    for await (let object of this.db.each<{hash: string}>(`SELECT DISTINCT hash AS hash FROM files WHERE data IS NULL`)){
       if(object.hash === "directory" || object.hash === null) continue;
       try{
         await fs.access(this.filepath(object), constants.R_OK)
       }catch(e){
         missing++;
-        console.log("Data :", object.data);
         console.error(`File ${object.hash} can not be read on disk`);
       }
+      await timers.setTimeout(1);
     }
     if(missing)console.error("found %d missing objects (can't fix)", missing);
   }
