959df3b4d096453d744e6cb7a9a08084c1ee9c0e
Sebastien DUMETZ <s.dumetz@holusion.com>
2025-09-02 09:14:03 +0200

    allow CORS requests on oembed and opensearch

--- source/server/routes/services/index.ts
+++ source/server/routes/services/index.ts
@@ -7,6 +7,12 @@ import { getEmbed } from "./oembed.js";
 
 
 const router = Router();
+router.use("/", (req, res, next)=>{
+  //CORS might be needed if someone wants to do client-side oembed or opensearch discovery
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Cache-Control", `max-age=${3600*24*30}, public`);
+  next();
+});
 
 router.get("/opensearch.xml", renderOpenSearch);
 router.get("/oembed", wrap(getEmbed));
