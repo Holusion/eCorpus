05ab6adee6aaf818349064104dbe81be0c2ad386
Sebastien DUMETZ <s.dumetz@holusion.com>
2025-10-03 09:45:22 +0200

    more control tasks

--- source/server/tasks/handlers/control/delay.ts
+++ source/server/tasks/handlers/control/delay.ts
@@ -7,14 +7,15 @@ import { randomInt } from 'node:crypto';
 interface DelayTaskParams{
   time: number;
   variance?:number;
+  value?:any;
 }
 
 /**
  * Apply a delay before resolving the task.
  * Can be useful with the variance parameter to spread-out heavy loads
  */
-export async function delayTask({task: {task_id, fk_scene_id:scene_id, data:{time, variance}}, signal, logger, context:{vfs, tasks}}:TaskHandlerParams<DelayTaskParams>){
+export async function delayTask({task: {task_id, fk_scene_id:scene_id, data:{time, variance, value}}, signal, logger, context:{vfs, tasks}}:TaskHandlerParams<DelayTaskParams>){
   let timeout = time + (variance?randomInt(variance):0);
   logger.debug("Start delay task with timeout: %d", timeout);
-  await timers.setTimeout(timeout, undefined, {signal});
+  return await timers.setTimeout(timeout, value, {signal});
 };
