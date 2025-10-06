eb2ddfc56c185624ef4343fc6c45d8c14cfd8216
Sebastien DUMETZ <s.dumetz@holusion.com>
2025-10-06 17:02:14 +0200

    better semantics for tasks grouping

--- source/server/tasks/handlers/control/delay.ts
+++ source/server/tasks/handlers/control/delay.ts
@@ -14,7 +14,7 @@ interface DelayTaskParams{
  * Apply a delay before resolving the task.
  * Can be useful with the variance parameter to spread-out heavy loads
  */
-export async function delayTask({task: {task_id, fk_scene_id:scene_id, data:{time, variance, value}}, signal, logger, context:{vfs, tasks}}:TaskHandlerParams<DelayTaskParams>){
+export async function delayTask({task: {task_id, fk_scene_id:scene_id, data:{time, variance, value}}, context:{signal, logger }}:TaskHandlerParams<DelayTaskParams>){
   let timeout = time + (variance?randomInt(variance):0);
   logger.debug("Start delay task with timeout: %d", timeout);
   return await timers.setTimeout(timeout, value, {signal});
--- source/server/tasks/handlers/control/groupOutputs.ts
+++ source/server/tasks/handlers/control/groupOutputs.ts
@@ -1,26 +1,27 @@
 import {  TaskDefinition, TaskHandlerParams } from "../../types.js";
 
 export interface GroupOutputsParams{
-  children: number[];
+  children?: number[];
 }
 
 /**
  * Returns all results once all child tasks have completed.
  * Do not use directly, but through the group method
  */
-export async function groupOutputsTasks({task:{fk_scene_id:scene_id, task_id, data:{ children }}, logger, context:{tasks}}:TaskHandlerParams<GroupOutputsParams>):Promise<number|any[]> {
-  if(!children) return [];
-  let outputs = [];
+export async function groupOutputsTask({task:{fk_scene_id:scene_id, task_id, after, parent, data:{ children }}, context:{logger, tasks}}:TaskHandlerParams<GroupOutputsParams>):Promise<number|any[]> {
+  let outputs= [];
+  let tracked =  new Set([...(children?? []), ...(after?? [])]);
+  if(typeof parent === "number") tracked.delete(parent);
+  logger.debug("Group outputs #%d", task_id, tracked);
+  for(let id of tracked){
+    let t = await tasks.getTask(id);
+    outputs.push(t.output);
+  }
 
-  logger.debug("Group outputs #%d", task_id);
   // Wait for children one by one :
   // We don't want to create too many event listeners
   // And we don't need to eagerly get results as soon as they are available
-  for(let task_id of children){
-    let result = await tasks.wait(task_id);
-    outputs.push(result);
-  }
-    logger.debug("Successfully group tasks :", task_id, outputs);
+    logger.debug("Successfully completed group tasks :", task_id, outputs);
   return outputs;
 };
 
