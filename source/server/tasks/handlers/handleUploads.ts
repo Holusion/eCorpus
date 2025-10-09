61e6ac773abb9b4cca833da0f2690a5f2f4dbbab
Sebastien DUMETZ <s.dumetz@holusion.com>
2025-10-09 16:12:44 +0200

    slightly improve upload task call signature

--- source/server/tasks/handlers/handleUploads.ts
+++ source/server/tasks/handlers/handleUploads.ts
@@ -5,6 +5,8 @@ import { TLanguageType } from "../../utils/schema/common.js";
 import uid from "../../utils/uid.js";
 import { createReadStream } from "node:fs";
 import { WriteFileParams } from "../../vfs/types.js";
+import { AssetDefinition } from "./createDocument.js";
+import { TDerivativeQuality } from "../../utils/schema/model.js";
 
 
 
@@ -13,6 +15,7 @@ interface UploadFileParams{
     path: string,
     name: string,
   }[],
+  optimize: boolean,
   scene_name: string,
   user_id: number,
   language?: TLanguageType,
@@ -20,10 +23,16 @@ interface UploadFileParams{
 /**
  * Analyze an uploaded file and create child task(s) accordingly
  */
-export async function handleUploads({task: {task_id, fk_scene_id:scene_id, data:{files, scene_name, user_id, language}}, context:{signal, logger, tasks}}:TaskHandlerParams<UploadFileParams>):Promise<number>{
+export async function handleUploads({task: {task_id, fk_scene_id:scene_id, data:{files, scene_name, user_id, language, optimize}}, context:{signal, logger, tasks}}:TaskHandlerParams<UploadFileParams>):Promise<number>{
+
+  //Do some preprocessing to check what we should generate?
+  /** @fixme handle other formats */
+  const modelFiles = files.filter(m=>m.name.toLowerCase().endsWith(".glb"));
+  const with_lod = 1 < modelFiles.length;
+  const qualities :TDerivativeQuality[] = with_lod? ["Thumb", "Low", "Medium", "High"]: ["Thumb", "High"];
 
   const group = await tasks.group(async function *(tasks){
-    for(let file of files){
+    for(let file of modelFiles){
       const ext = path.extname(file.name);
       const basename = path.basename(file.name, ext);
       const extname = ext.slice(1).toLowerCase();
@@ -33,12 +42,13 @@ export async function handleUploads({task: {task_id, fk_scene_id:scene_id, data:
       if(extname === "glb"){
         logger.debug("Found a glb file: "+file.name);
         let model_id = uid();
-        for(let quality of ["High", "Medium", "Low", "Thumb"]){
+
+        for(let quality of qualities){
           logger.debug("Create processing tasks for %s in quality %s", file.name, quality);
           const filename = `${basename}_${quality.toLowerCase()}.glb`;
-          let optimize = await tasks.create({type: "optimizeGlb", data: {file: file.path}});
-          
-          let parse = await tasks.create({type: "parseGlbTask", data: {file: "$[0]"}, after: [optimize]});
+          let optimizeTask = (quality == "High"  && optimize)? null: await tasks.create({type: "optimizeGlb", data: {file: file.path}});
+
+          let parse = await tasks.create({type: "parseGlbTask", data: {file: optimizeTask?"$[0]":file.path}, after: optimizeTask?[optimizeTask]: null});
 
           let copy = await tasks.create({
             type: "copyFileTask",
@@ -47,22 +57,22 @@ export async function handleUploads({task: {task_id, fk_scene_id:scene_id, data:
               name: filename,
               mime: "model/gltf-binary",
               user_id,
-              filepath: '$[0]' as any,
+              filepath: optimizeTask?'$[0]' as any: file.path,
             },
-            after: [optimize]
+            after: optimizeTask?[optimizeTask]: null
           });
+
           yield await tasks.create({
             type: "groupOutputsTask",
             data: {
               id: model_id,
               quality,
               usage: "Web3D",
-              filepath: "$[0]",
               filename,
-              meta: "$[1]",
-              size: "$[2].size"
+              meta: "$[0]",
+              size: "$[1].size"
             },
-            after: [optimize, parse, copy],
+            after: [parse, copy],
           });
         }
       }else{
