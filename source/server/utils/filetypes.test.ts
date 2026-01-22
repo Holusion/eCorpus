fdb5af8f9e3527ad1272bf7c180922148eafddfe
Sebastien DUMETZ <s.dumetz@holusion.com>
2026-01-22 12:21:02 +0100

    improve filetypes probbing and add useful methods

--- source/server/utils/filetypes.test.ts
+++ source/server/utils/filetypes.test.ts
@@ -1,7 +1,10 @@
+import path from "node:path";
 import express from "express";
-import { compressedMime, getContentType, getMimeType } from "./filetypes.js";
+import { compressedMime, extFromType, getContentType, getMimeType, parseMagicBytes, readMagicBytes } from "./filetypes.js";
 import request from "supertest";
 
+
+import { fixturesDir } from "../__test_fixtures/fixtures.js";
 
 describe("getMimeType",function(){
   //Check proper operation of mimetype guessing function
@@ -92,5 +95,77 @@ describe("compressedMime", function(){
     it(`${t}: true`, function(){
       expect(compressedMime(t)).to.be.true;
     });
+  });
+});
+
+
+describe("extFromType()", function(){
+
+  Object.entries({
+    "model/gltf-binary": ".glb",
+    "image/jpeg": ".jpeg",
+    "application/zip": ".zip",
+    "application/si-dpo-3d.document+json": ".svx.json",
+  }).forEach(function([type, extname]){
+    it(`${type} => ${extname}`, function(){
+      expect(extFromType(type)).to.equal(extname);
+    });
+  });
+
+  it("returns an empty string for extensionless files", function(){
+    expect(extFromType("application/octet-stream")).to.equal("");
+  });
+});
+
+describe("parseMagicBytes()", function(){
+  //Header of a 1156x420 png
+  //Header of a 1067x800 jpeg
+  it("image/png", function(){
+    expect(parseMagicBytes(Buffer.from('89504e470d0a1a0a', "hex"))).to.equal("image/png");
+  });
+
+  it("image/jpeg", function(){
+    [
+      "FFD8FFDB", //Raw jpeg
+      "FFD8FFE000104A4649460001", //JFIF
+      "FFD8FFEE", //Also jpeg
+      "FFD8FFE0", // still jpeg
+    ].forEach((str)=>{
+      expect(parseMagicBytes(Buffer.from(str, "hex")), `0x${str} should be a valid jpeg header`).to.equal("image/jpeg")
+    })
+  });
+  it("image/webp", function(){
+    //Header with a size of 0
+    expect(parseMagicBytes(Buffer.from('524946460000000057454250', 'hex'))).to.equal("image/webp");
+    //header with a real size
+    expect(parseMagicBytes(Buffer.from('52494646b254000057454250', 'hex'))).to.equal("image/webp");
+    //header from another RIFF container
+    expect(parseMagicBytes(Buffer.from('524946460000000057415645', 'hex'))).to.equal("application/octet-stream");
+  });
+
+  it("model/gltf-binary", function(){
+    // See https://registry.khronos.org/glTF/specs/2.0/glTF-2.0.html#binary-header
+    // maps to glTF ascii string, but backwards, because it's a little-endian format
+    const b= Buffer.alloc(4);
+    b.writeUint32LE(0x46546C67);
+    expect(parseMagicBytes(b)).to.equal("model/gltf-binary");
+  });
+
+  it("application/zip", function(){
+    // See https://en.wikipedia.org/wiki/ZIP_(file_format)#File_headers
+    const b = Buffer.alloc(4);
+    b.writeUint32LE(0x04034b50);
+    expect(parseMagicBytes(b)).to.equal("application/zip");
+  });
+});
+
+describe("readMagicBytes()", function(){
+  it("reads first bytes of a glb file", async function(){
+    let type = await readMagicBytes(path.join(fixturesDir, "cube.glb"));
+    expect(type).to.equal("model/gltf-binary");
+  });
+
+  it("throws if file doesn't exist", async function(){
+    await expect(readMagicBytes(path.join(fixturesDir, "not_cube.glb"))).to.be.rejectedWith({code: "ENOENT"} as any);
   });
 });
