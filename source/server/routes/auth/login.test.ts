b5bb7388dc626c678b9872bd81ee0252b7100aa5
Sebastien DUMETZ <s.dumetz@holusion.com>
2026-04-24 15:26:51 +0200

    fixup: restore session max age after login tests

--- source/server/routes/auth/login.test.ts
+++ source/server/routes/auth/login.test.ts
@@ -11,16 +11,19 @@ import { BadRequestError, ForbiddenError } from "../../utils/errors.js";
 
 describe("/auth/login", function(){
   let vfs :Vfs, userManager :UserManager, user :User, admin :User;
+  let _maxAge: number;
   this.beforeAll(async function(){
     let locals = await createIntegrationContext(this);
     vfs = locals.vfs;
     userManager = locals.userManager;
+    _maxAge = locals.sessionMaxAge;
   });
   this.afterAll(async function(){
     await cleanIntegrationContext(this);
   });
   this.beforeEach(async function(){
     await resetIntegrationContext(this);
+    this.server.locals.sessionMaxAge = _maxAge;
     user = await userManager.addUser("bob", "12345678");
     admin = await userManager.addUser("alice", "12345678", "admin");
   });
